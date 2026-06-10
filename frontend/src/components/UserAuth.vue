8127edef95fd5e9576275b91d059c0169f11a51f
YohanCollado <yohancollado@yahoo.com>
2026-06-10 19:34:52 -0400

    Implementation of Authentication and User Management was completed

--- frontend/src/components/UserAuth.vue
+++ frontend/src/components/UserAuth.vue
@@ -64,6 +64,7 @@
 </template>
 
 <script>
+const $ = window.jQuery
 export default {
   data () {
     return {
@@ -71,8 +72,37 @@ export default {
       username: '',
       password: ''
     }
+  },
+
+  methods: {
+    signUp () {
+      $.post('http://localhost:8000/auth/users/create/',
+        this.$data, (data) => {
+          alert('Your account has been created. You will be signed in automatically')
+          this.SignIn()
+        }).fail((response) => {
+        alert(response.responseText)
+      })
+    },
+
+    signIn () {
+      const credentials = {
+        username: this.username,
+        password: this.password
+      }
+      $.post('http://localhost:8000/auth/token/create/',
+        credentials, (data) => {
+          sessionStorage.setItem('authToken', data.auth_token)
+          sessionStorage.setItem('username', this.username)
+          this.$router.push('/chats')
+        })
+        .fail((response) => {
+          alert(response.responseText)
+        })
+    }
   }
 }
+
 </script>
 
 <style scoped>
